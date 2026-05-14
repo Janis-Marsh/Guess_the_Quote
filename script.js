1f4b5b6db74d71c88846f08f3d59462d200d3aa4
Janis-Marsh <janismarshall23@gmail.com>
2026-05-14 00:17:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
 // Stores all quotes
 const quotes = [
-    { quote: "this sign can't stop me cause i can't read!", answer: "Arthur", category: "tv show" },
-    { quote: "So remember, whether a bot is made of new parts, old parts, or spare parts, you can shine no matter what you do.", answer: "Robots", category: "movie" },
-    { quote: "This is America, Lois. Men have always run things and there have never been any problems, whatsoever. And don't say the economy or Iraq or income inequality or racism or Brett Kavanaugh or air pollution or Vietnam or slavery or Watergate or capitalism or #MeToo or homelessness or police brutality or homophobia or Monica Lewinsky or school shootings or Native American genocide or FOX News or Tim Allen or climate change.", answer: "Family Guy", category: "tv show" },
-    { quote: "Ya like jazz?", answer: "Bee Movie", category: "movie" },
-    { quote: "It's never lupus.", answer: "House MD", category: "tv show" },
-    { quote: "I don't trust the guy. He's like an eel dipped in grease, swimming in motor oil.", answer: "Total Drama World Tour", category: "tv show" },
-    { quote: "I just washed my hands that's why they're wet. No other reason.", answer: "Spider-man: Into the Spider-verse", category: "movie" },
-    { quote: "I'm the one who gripped you tight and raised you from Perdition.", answer: "Supernatural", category: "tv show" },
-    { quote: "My ex-wife still misses me... but her aim is getting better!", answer: "Gravity Falls", category: "tv show" },
-    { quote: "I'm sorry, but I believe I squashed a frog when I fell.", answer: "The Apothecary Diaries", category: "tv show" },
-    { quote: "My first word was Hoot. My second word was hoot hoot. My third word...", answer: "The Owl House", category: "tv show" },
-    { quote: "We are going to steal... pause for effect... THE MOON!!!", answer: "Despicable Me", category: "movie" },
-    { quote: "How long have you been 17?", answer: "Twilight", category: "movie" },
-    { quote: "Live long and prosper.", answer: "Star Trek", category: "tv show" },
-    { quote: "Flash, Flash, Hundred Yard Dash", answer: "Zootopia", category: "movie" },
-    { quote: "This is where the fun begins", answer: "Star Wars: Episode III - Revenge of the Sith", category: "movie" },
-    { quote: "What did you just say? Chai tea? Chai means 'tea', bro! You are saying 'tea tea'! Would I ask you for a 'coffee coffee' with room for 'cream cream'?", answer: "Spider-man: Across the spider-verse", category: "movie" },
-    { quote: "My own mother thought I was a monster... She was right, of course, but it still hurt", answer: "Avatar: The Last Airbender", category: "tv show" },
-    { quote: "Ninja Storm, Ranger Form!!!!", answer: "Power Rangers Ninja Storm", category: "tv show" },
-    { quote: "Skadoosh", answer: "Kung Fu Panda", category: "movie" },
-    { quote: "Inner Peace, Dinner Please, Dinner with Peas", answer: "Kung Fu Panda 2", category: "movie" },
-    { quote: "This is a cow farm. You're gonna find cows outside.", answer: "Barnyard", category: "movie" },
-    { quote: "You just gestured to all of me.", answer: "How to train your dragon", category: "movie" },
-    { quote: "The Egyptians believe that the most significant thing you could do in your life was die.", answer: "Cunk on Earth", category: "tv show" },
-    { quote: "You want my money, go get it!", answer: "John Mulaney: Kis Gorgeous at Radio City", category: "comedy" },
-    { quote: "I don't know. I ... I guess I'm not woke. Okay? Fine! You win with your gay stuff! That's what you want, right? To win", answer: "Family Guy", category: "tv show" },
-    { quote: "Ha ha ha ha ha! Ha ha ha ha ha! Hey, look at that high-waisted man! He got feminine hips!", answer: "John Mulaney: New in Town", category: "comedy" },
-    { quote: "Hey, you could pour soup in my lap and I'll probably apologize to you.", answer: "John Mulaney: The Comeback Kid", category: "comedy" },
-    { quote: "Hey, before we hand you a baby forever, can you put this metal bowl in the microwave for me?", answer: "Taylor Tomlinson: Quarter-Life Crisis", category: "comedy" },
-    { quote: "When I can't sleep, I lay on the floor in the kitchen for 10 minutes", answer: "Taylor Tomlinson: Have It All", category: "comedy" },
-    { quote: "She's in heaven, I'm on Netflix, it all worked out.", answer: "Taylor Tomlinson: Look at You", category: "comedy" },
-    { quote: "And remember, with a little Rust-eze, and an insane amount of luck... You too, can look like me. Kachow.", answer: "Cars", category: "movie" },
+    { quote: "this sign can't stop me cause i can't read!", answer: "Arthur", category: "tv show", hint: "D.W Read" },
+    { quote: "So remember, whether a bot is made of new parts, old parts, or spare parts, you can shine no matter what you do.", answer: "Robots", category: "movie", hint: "Bigweld" },
+    { quote: "This is America, Lois. Men have always run things and there have never been any problems, whatsoever. And don't say the economy or Iraq or income inequality or racism or Brett Kavanaugh or air pollution or Vietnam or slavery or Watergate or capitalism or #MeToo or homelessness or police brutality or homophobia or Monica Lewinsky or school shootings or Native American genocide or FOX News or Tim Allen or climate change.", answer: "Family Guy", category: "tv show", hint: "Peter Griffin" },
+    { quote: "Ya like jazz?", answer: "Bee Movie", category: "movie", hint: "Barry B. Benson" },
+    { quote: "It's never lupus.", answer: "House MD", category: "tv show", hint: "Gregory House" },
+    { quote: "I don't trust the guy. He's like an eel dipped in grease, swimming in motor oil.", answer: "Total Drama World Tour", category: "tv show", hint: "Noah" },
+    { quote: "I just washed my hands that's why they're wet. No other reason.", answer: "Spider-man: Into the Spider-verse", category: "movie", hint: "Peter Porker" },
+    { quote: "I'm the one who gripped you tight and raised you from Perdition.", answer: "Supernatural", category: "tv show", hint: "Castiel" },
+    { quote: "My ex-wife still misses me... but her aim is getting better!", answer: "Gravity Falls", category: "tv show", hint: "Stan Pines" },
+    { quote: "I'm sorry, but I believe I squashed a frog when I fell.", answer: "The Apothecary Diaries", category: "tv show", hint: "Maomao" },
+    { quote: "My first word was Hoot. My second word was hoot hoot. My third word...", answer: "The Owl House", category: "tv show", hint: "Hooty" },
+    { quote: "We are going to steal... pause for effect... THE MOON!!!", answer: "Despicable Me", category: "movie", hint: "Felonious Gru" },
+    { quote: "How long have you been 17?", answer: "Twilight", category: "movie", hint: "Bella Swan" },
+    { quote: "Live long and prosper.", answer: "Star Trek", category: "tv show", hint: "Spock" },
+    { quote: "Flash, Flash, Hundred Yard Dash", answer: "Zootopia", category: "movie", hint: "Nick Wilde" },
+    { quote: "This is where the fun begins", answer: "Star Wars: Episode III - Revenge of the Sith", category: "movie", hint: "Anakin Skywalker" },
+    { quote: "What did you just say? Chai tea? Chai means 'tea', bro! You are saying 'tea tea'! Would I ask you for a 'coffee coffee' with room for 'cream cream'?", answer: "Spider-man: Across the spider-verse", category: "movie", hint: "Pavitr Prabhakar" },
+    { quote: "My own mother thought I was a monster... She was right, of course, but it still hurt", answer: "Avatar: The Last Airbender", category: "tv show", hint: "Azula" },
+    { quote: "Ninja Storm, Ranger Form!!!!", answer: "Power Rangers Ninja Storm", category: "tv show", hint: "Wind Rangers" },
+    { quote: "Skadoosh", answer: "Kung Fu Panda", category: "movie", hint: "Po" },
+    { quote: "Inner Peace, Dinner Please, Dinner with Peas", answer: "Kung Fu Panda 4", category: "movie", hint: "Po" },
+    { quote: "This is a cow farm. You're gonna find cows outside.", answer: "Barnyard", category: "movie", hint: "Nathan Randall Beady III" },
+    { quote: "You just gestured to all of me.", answer: "How to train your dragon", category: "movie", hint: "Hiccup Horrendous Haddock III" },
+    { quote: "Thank you for nothing, you useless reptile.", answer: "How to train your dragon", category: "movie", hint: "Hiccup Horrendous Haddock III" },
+    { quote: "The Egyptians believe that the most significant thing you could do in your life was die.", answer: "Cunk on Earth", category: "tv show", hint: "Philomena Cunk" },
+    { quote: "You want my money, go get it!", answer: "John Mulaney: Kid Gorgeous at Radio City", category: "comedy", hint: "John Mulaney" },
+    { quote: "I don't know. I ... I guess I'm not woke. Okay? Fine! You win with your gay stuff! That's what you want, right? To win", answer: "Family Guy", category: "tv show", hint: "Lois Griffin" },
+    { quote: "Ha ha ha ha ha! Ha ha ha ha ha! Hey, look at that high-waisted man! He got feminine hips!", answer: "John Mulaney: New in Town", category: "comedy", hint: "John Mulaney" },
+    { quote: "Hey, you could pour soup in my lap and I'll probably apologize to you.", answer: "John Mulaney: The Comeback Kid", category: "comedy", hint: "John Mulaney" },
+    { quote: "Hey, before we hand you a baby forever, can you put this metal bowl in the microwave for me?", answer: "Taylor Tomlinson: Quarter-Life Crisis", category: "comedy", hint: "Taylor Tomlinson" },
+    { quote: "When I can't sleep, I lay on the floor in the kitchen for 10 minutes", answer: "Taylor Tomlinson: Have It All", category: "comedy", hint: "Taylor Tomlinson" },
+    { quote: "She's in heaven, I'm on Netflix, it all worked out.", answer: "Taylor Tomlinson: Look at You", category: "comedy", hint: "Taylor Tomlinson" },
+    { quote: "And remember, with a little Rust-eze, and an insane amount of luck... You too, can look like me. Kachow.", answer: "Cars", category: "movie", hint: "Lighting McQueen" },
 ];
 
 // global variables
@@ -57,6 +58,7 @@ let activeQuotes = [];
 // Gets important HTML element ids
 const guessInput = document.getElementById('guessInput');
 const feedback = document.getElementById('feedback');
+const hint = document.getElementById('hint');
 const tracker = document.getElementById('scoreTracker');
 
 // Randomly shuffles array
@@ -143,6 +145,13 @@ function checkGuess(){
         feedback.style.color = 'red';
     }
 
+    // Gives a hint after 2 guesses
+    if (guesses >= 2) {
+        hint.textContent = `Hint: Said by ${shuffledQuote[currentIndex].hint}`
+        hint.style.color = 'orange';
+        hint.style.fontWeight = 'bold';
+    }
+
     // Reveals answer after too many wrong guesses
     if (guesses >= 3) {
         feedback.textContent = `Correct Answer: ${shuffledQuote[currentIndex].answer}`;
@@ -169,6 +178,7 @@ function nextQuote() {
 
     displayQuote();
     feedback.textContent = '';
+    hint.textContent = '';
 }
 
 // handles the end of the game
@@ -183,6 +193,7 @@ function gameOver() {
 function resetGame() {
 
     feedback.textContent = '';
+    hint.textContent = '';
     tracker.textContent = '';
     tracker.className = '';
 
